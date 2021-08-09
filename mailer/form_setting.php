f346f725463c8fb6529cd5dfe34d109639eed635
Muhammad Safwan Yasin <safwanyasin2002@gmail.com>
2021-08-10 03:51:53 +0500

    first commit

---
+++ mailer/form_setting.php
@@ -0,0 +1,8 @@
+<?php
+	/*Form settings*/
+	$subj = "New message from your portfolio site"; //letter subject
+	$to = 'safwanyasin2002@gmail.com'; // Enter Your E-mail
+	$from = 'safwanyasin2002@gmail.com'; // Admin e-mail
+	$fromName = 'SocioForge'; // Your company name
+	$charset = 'UTF-8';
+?>
