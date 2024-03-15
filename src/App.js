7eddb09fa0b974a179db09a4c7997088a1e49616
M Safwan Yasin <safwanyasin2002@gmail.com>
2024-03-15 03:38:00 +0300

    minor changes

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
   }, []);
   return (
     <Routes>
-      <Route path="/home" element={<Layout />}>
+      <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         {/* <Route path="about" element={<AboutMe />} /> */}
       </Route>
